23c20dba060f7c400d8e8b9a4b0f762abb36d803
Erik Pettersson <mrsnille@gmail.com>
2019-10-29 10:16:10 +0100

    Trying to add flip flop flr the secret open button.

--- start.py
+++ start.py
@@ -22,14 +22,21 @@ kit.servo[0].angle = 15
 goal = 10
 
 # Pin used for the lidswitch. Low when the lid is closed.
-lidpin = 27
+lidpin = 5
+
+# Secret Open Pin
+secretpin = 27
+
+# Defines if to open using the secret button.
+open = 0
 
 # Ratio in House: 855
 ratio = 855
 # Ratio in Workshop: 827
 #ratio = 827
 
-
+# Play the sound
+read = 1
 
 # The directory where the sounds are located.
 sounddir = "Sounds"
@@ -56,6 +63,7 @@ try:
         print('invalid data', reading)
     # Setting up the input button.
     GPIO.setup(lidpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(secretpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
     hx.set_scale_ratio(ratio)
@@ -64,19 +72,44 @@ try:
 
         if GPIO.input(lidpin) == GPIO.LOW:
             if read == 1:
+                print("Collected coins so far: " + str(val))
                 # Do the logic here!
                 fullpath = sounddir + "/" + str(val) + ".wav"
                 # print(fullpath)
                 if path.exists(fullpath):
+                    os.system("aplay -q " + sounddir + "/Count-01.wav")
                     os.system("aplay -q " + fullpath)
-                print("Collected coins so far: " + str(val))
+                    os.system("aplay -q " + sounddir + "/Count-02.wav")
                 read = 0
 #                time.sleep(3)
                 if val == goal:
                     kit.servo[0].angle = 130
 
+        # If secret button is pressed unlock or lock the chest.
+        if GPIO.input(secretpin) == GPIO.LOW:
+            
+            # If open is 0 open the lock.
+            if open = 0
+                kit.servo[0].angle = 130
+
+#            open = 1
+ 
+
+
+       else:
+            kit.servo[0].angle = 15
+
+#            open = 1
+ 
+
+        # The lid has to be opened and closed for the sounds to play again.
         if GPIO.input(lidpin) == GPIO.HIGH:
             read = 1
+
+        # 
+        if GPIO.input(secretpin) == GPIO.HIGH:
+            open = 0
+
 
 except (KeyboardInterrupt, SystemExit):
     print('Bye :)')
