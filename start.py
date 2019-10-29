68fdf2e5e7aab6da8d743301384ef1c5671486db
Erik Pettersson <mrsnille@gmail.com>
2019-10-29 14:27:19 +0100

    It's starting to take form...

--- start.py
+++ start.py
@@ -3,8 +3,8 @@
 
 import RPi.GPIO as GPIO  # import GPIO
 from hx711 import HX711  # import the class HX711
-import time
-from time import sleep
+#import time
+#from time import sleep
 import sys
 import os
 import os.path
@@ -13,10 +13,16 @@ from adafruit_servokit import ServoKit
 
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+kit = ServoKit(channels=16)
 
-# Locking chest.
-kit.servo[0].angle = 15
+# Servo locking chest angle.
+lockangle = 15
+
+# Servo unlocking chest angle.
+unlockangle = 130
+
+# Start number for information sample (1, 2, 3).
+info = 0
 
 # Coins to collect for the chest to open.
 goal = 10
@@ -24,15 +30,12 @@ goal = 10
 # Pin used for the lidswitch. Low when the lid is closed.
 lidpin = 5
 
-# Secret Open Pin
+# Secret open switch Pin
 secretpin = 27
 
-# Defines if to open using the secret button.
-open = 0
-
-# Ratio in House: 855
+# Coin weight number ratio in House: 855
 ratio = 855
-# Ratio in Workshop: 827
+# Coin weight number ratio in Workshop: 827
 #ratio = 827
 
 # Play the sound
@@ -40,6 +43,10 @@ read = 1
 
 # The directory where the sounds are located.
 sounddir = "Sounds"
+
+# Loking the chest
+print("Locking the chest")
+kit.servo[0].angle = lockangle
 
 
 try:
@@ -55,64 +62,79 @@ try:
         raise ValueError('Tare is unsuccessful.')
 
     reading = hx.get_raw_data_mean()
-    if reading:  # always check if you get correct value or only False
+#    if reading:  # always check if you get correct value or only False
         # now the value is close to 0
-        print('Data subtracted by offset but still not converted to units:',
-              reading)
-    else:
-        print('invalid data', reading)
-    # Setting up the input button.
+#        print('Data subtracted by offset but still not converted to units:', reading)
+#    else:
+#        print('invalid data', reading)
+
+    # Setting up the input buttons.
     GPIO.setup(lidpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(secretpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
+    print("I will now start to keep track of collected coins in an infinite loop. To exit press 'CTRL + C' or collect", goal, "coins to unlock the chest")
+
+    # Calculating the scale of one sigle coin.
     hx.set_scale_ratio(ratio)
+
+    # Staring the loop.
     while True:
+
+        # Chacks what's on the scale (returns only intigers and no negative numbers).
         val = max(0, int(hx.get_weight_mean(5)))
 
+        # When lid opens, this happens...
         if GPIO.input(lidpin) == GPIO.LOW:
             if read == 1:
                 print("Collected coins so far: " + str(val))
-                # Do the logic here!
+                if val == 0:
+                    # Avoids the first message to be plyed when the program starts with the lid closed.
+                    if info == 0:
+                        info = 1
+                     # Plays message 1 -> Next 2
+                    elif info == 1:
+                        os.system("aplay -q " + sounddir + "/Explain-01.wav")
+                        info = 2
+                    # Plays message 2 -> Next 3
+                    elif info == 2:
+                        os.system("aplay -q " + sounddir + "/Explain-02.wav")
+                        info = 3
+                    # Plays message 3 -> Next 1
+                    elif info == 3:
+                        os.system("aplay -q " + sounddir + "/Explain-03.wav")
+                        info = 1
+
+                # Set the path and filename to the number sound.
                 fullpath = sounddir + "/" + str(val) + ".wav"
-                # print(fullpath)
+
+                # If the sound exists, play it.
                 if path.exists(fullpath):
                     os.system("aplay -q " + sounddir + "/Count-01.wav")
                     os.system("aplay -q " + fullpath)
                     os.system("aplay -q " + sounddir + "/Count-02.wav")
+
+                # Only play once. The lid has to be closed and opend again to play again.
                 read = 0
-#                time.sleep(3)
+
+                # If the goal amount of coins are reached. Open the chest.
                 if val == goal:
-                    kit.servo[0].angle = 130
+                    raise SystemExit
 
-        # If secret button is pressed unlock or lock the chest.
+        # If secret button is pressed unlock the chest as long as the button is pressed.
         if GPIO.input(secretpin) == GPIO.LOW:
-            
-            # If open is 0 open the lock.
-            if open = 0
-                kit.servo[0].angle = 130
-
-#            open = 1
- 
-
-
-       else:
-            kit.servo[0].angle = 15
-
-#            open = 1
- 
+            kit.servo[0].angle = unlockangle
+        else:
+            kit.servo[0].angle = lockangle
 
         # The lid has to be opened and closed for the sounds to play again.
         if GPIO.input(lidpin) == GPIO.HIGH:
             read = 1
 
-        # 
-        if GPIO.input(secretpin) == GPIO.HIGH:
-            open = 0
-
-
+# At the end of everything, exit cleanly.
 except (KeyboardInterrupt, SystemExit):
-    print('Bye :)')
+    kit.servo[0].angle = unlockangle
+    print("Chest is now unlocked, bye...")
 
+# Clean up the inputs.
 finally:
     GPIO.cleanup()
