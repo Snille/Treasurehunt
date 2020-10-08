b5ce09d3cb0f6a98eeed4e90541c20574658c40a
Erik Pettersson <mrsnille@gmail.com>
2020-10-09 00:15:05 +0200

    Cleaned up alot and added more features. Moves alla the messages to the top of the script.

--- start.py
+++ start.py
@@ -34,6 +34,7 @@ unlockangle = 130
 info = 1
 
 # Coins to collect for the chest to open (100).
+#goal = 100
 goal = 5
 
 # Set to 1 if it is ok to add more coins then specified in goal (chest opens when goal or higher is reached) otherwise it must be the exact goal number.
@@ -42,7 +43,7 @@ overgoalok = 1
 # Pin used for the lid switch. Low when the lid is closed.
 lidpin = 23
 
-# Secret open switch Pin
+# Secret open switch Pin.
 secretpin = 24
 
 # Coin weight number ratio in House: 855
@@ -50,70 +51,101 @@ ratio = 855
 # Coin weight number ratio in Workshop: 827
 #ratio = 827
 
-# Play the sound (0 for no 1 for yes)
+# Play the chest sounds (0 for no 1 for yes).
 playsound = 1
 
-# Time to wait before going back to the "normal" profile on the mirror
+# Time to wait before going back to the "normal" profile on the MagicMirror.
 #waittime = 300 # Five minutes.
 waittime = 10 # 10 seconds. 
 
 # MagicMirror IP and port.
-#mmip = "10.0.0.20" # Development MagicMirror.
-mmip = "10.0.0.112" # Actual MagicMirror.
-mmport = "8080" 
+mmip = "10.0.0.20" # Development MagicMirror.
+#mmip = "10.0.0.112" # Actual MagicMirror.
+mmport = "8181"
+#mmport = "8080"
 
-# Post to the MagicMirror (1 = on or 0 = off)
+# MagicMirror MMM-Remote-Control module commands to build on.
+profilechange = "http://" + mmip + ":" + mmport + "/remote?action=NOTIFICATION&notification=CURRENT_PROFILE&payload="
+showmodule = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module="
+hidemodule = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module="
+
+# Post to the MagicMirror (1 = on or 0 = off).
 mmpost = 1
 
-# Seconds to show the message on the MagicMirror (shows until time is up or a new message is sent).
+# Seconds to show the IFTTT messages on the MagicMirror (shows until time is up or a new message is sent).
 mmtime = 3600 # For each message.
-mmendtime = 60 # For the last message.
+mmendtime = 300 # For the last message.
 
-# Size of the MagicMirror message (small, medium, large)
+# Size of the MagicMirror IFTTT message (small, medium, large).
 mmsize = "large"
 
-# MagicMirror Party Profile (set to the MMM-Remote-Control module that changes profile via MMM-ProfileSwitcher)
-startprofile = "http://" + mmip + ":" + mmport + "/remote?action=NOTIFICATION&notification=CURRENT_PROFILE&payload=Skattjakt"
-endprofile = "http://" + mmip + ":" + mmport + "/remote?action=NOTIFICATION&notification=CURRENT_PROFILE&payload=Grattis"
-normalprofile = "http://" + mmip + ":" + mmport + "/remote?action=NOTIFICATION&notification=CURRENT_PROFILE&payload=Louise"
+# MagicMirror treasure hunt Profile (sent to the MMM-Remote-Control module that changes profile via MMM-ProfileSwitcher).
+startprofile = profilechange + "Skattjakt"
+# MagicMirror treasure hunt win profile.
+endprofile = profilechange + "Grattis"
+# MagicMirror "normal" profile.
+normalprofile = profilechange + "Louise"
 
-# MagicMirror modules MMM-IFTTTs URL to use.
-url="http://" + mmip + ":" + mmport + "/IFTTT"
+# MagicMirror MMM-IFTTTs modules URL to use.
+ifttturl = "http://" + mmip + ":" + mmport + "/IFTTT"
 
-# MagicMirror modules to show / hide
-# Module to show 
-#hideallmodules = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=all"
-#showiftttmodule = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_51_MMM-IFTTT"
+# MagicMirror modules to show / hide stuff.
+# Shows and hides the IFTTT module at the end.
+showiftttmodule = showmodule + "module_52_MMM-IFTTT"
+hideiftttmodule = hidemodule + "module_52_MMM-IFTTT"
 
-# Shows: http://localhost/img/magicmirror/decoration/pirate-with-flag.png
-showprestartmodule = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_13_MMM-ImageFit"
-hideprestartmodule = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_13_MMM-ImageFit"
+# Shows and hides: http://localhost/img/magicmirror/decoration/pirate-with-flag.png
+showprestartmodule = showmodule + "module_13_MMM-ImageFit"
+hideprestartmodule = hidemodule + "module_13_MMM-ImageFit"
 
-# Shows: http://localhost/img/magicmirror/decoration/coin-zoom-cropped.gif
-showstartmodule = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_14_MMM-ImageFit"
-hidestartmodule = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_14_MMM-ImageFit"
+# Shows and hides: http://localhost/img/magicmirror/decoration/coin-zoom-cropped.gif
+showstartmodule = showmodule + "module_14_MMM-ImageFit"
+hidestartmodule = hidemodule + "module_14_MMM-ImageFit"
 
-# Shows: http://localhost/img/magicmirror/decoration/coin-fall.gif
-showcoinmodule1 = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_15_MMM-ImageFit"
-hidecoinmodule1 = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_15_MMM-ImageFit"
+# Shows and hides: http://localhost/img/magicmirror/decoration/coin-fall.gif
+showcoinmodule1 = showmodule + "module_15_MMM-ImageFit"
+hidecoinmodule1 = hidemodule + "module_15_MMM-ImageFit"
 
-# Shows: http://localhost/img/magicmirror/decoration/chest-on-coins.png
-showcoinmodule2 = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_16_MMM-ImageFit"
-hidecoinmodule2 = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_16_MMM-ImageFit"
+# Shows and hides: http://localhost/img/magicmirror/decoration/chest-on-coins.png
+showcoinmodule2 = showmodule + "module_16_MMM-ImageFit"
+hidecoinmodule2 = hidemodule + "module_16_MMM-ImageFit"
 
 # Hides the "everyone modules".
-hidemodulebar = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_57_MMM-Modulebar"
-hidehideall = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_59_MMM-HideAll"
-hidetouchnavigation = "http://" + mmip + ":" + mmport + "/remote?action=HIDE&module=module_60_MMM-TouchNavigation"
+hidemodulebar = hidemodule + "module_57_MMM-Modulebar"
+hidehideall = hidemodule + "module_59_MMM-HideAll"
+hidetouchnavigation = hidemodule + "module_60_MMM-TouchNavigation"
+hidecurrentweather = hidemodule + "module_36_currentweather"
+hideweatherforecast = hidemodule + "module_38_weatherforecast"
 
 # Shows the "everyone modules".
-showmodulebar = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_57_MMM-Modulebar"
-showhideall = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_59_MMM-HideAll"
-showtouchnavigation = "http://" + mmip + ":" + mmport + "/remote?action=SHOW&module=module_60_MMM-TouchNavigation"
+showmodulebar = showmodule + "module_57_MMM-Modulebar"
+showhideall = showmodule + "module_59_MMM-HideAll"
+showtouchnavigation = showmodule + "module_60_MMM-TouchNavigation"
+showcurrentweather = showmodule + "module_36_currentweather"
+showweatherforecast = showmodule + "module_38_weatherforecast"
 
+## IFTTT messages sent to the MagicMirror.
+# Pre start message, displayed when the hunt begins and the chest locks.
+iftttprestartmessage = "Jakten på piratmynten har börjat!!"
+# End message, displayed when the hunt is over and the chest unlocks. 
+iftttendmessage = "Grattis!! Ni har hittat mer än 100 piratmynt! Kistan är upplåst, Hoppas godiset smakar!"
+# Start message, displays when the lid is opened for the first time and until the first coin is added.
+iftttstartmessage = "För att låsa upp kistan helt måste ni samla ihop minst 100 piratmynt och peta in dem genom glipan som blir när man försöker öppna kistlocket!"
+# Repeated message when coins are added (two parts). The collected number of coins is inserted in between the messages.
+iftttcoinmessage = ["Ni har hittat ", " av 100 piratmynt. Fortsätt leta..."]
 
-## Sonos control commands.
-# Set to one if you want to use a Sonos player.
+## Console messages.
+# Pre start message, displayed when the script locks the chest.
+consoleprestartmessae = "The treasure hunt has started! Locking the chest."
+# Start message in two parts when the chest is locked and waiting. The goal number is added in between the messages.
+consolestartmessage = ["Chest is locked, I will now keep track of collected coins in an infinite loop. To exit press CTRL + C or collect","coins to unlock the chest."]
+# Repeated message when coins are added.
+consolecoinmessage = "Collected coins so far: "
+# End message, when the hunt is over.
+consoleendmessage = "Chest is now unlocked, I hope you hade a good hunt! :)"
+
+## SONOS control commands.
+# Set to 1 if you want to use a SONOS player.
 playonsonos = 1
 # What player (room)
 player = "kontor"
@@ -122,16 +154,11 @@ sonosapiip = "10.0.0.21"
 # Port to the SONOS http API.
 sonosapiport= "5005"
 # Volume to play on.
-volume = "15"
+volume = 20
 # Playlist name
 playlist = "Skattjakt"
 
-
-setvol = "http://" sonosapiip + ":" + sonosapiport + "/" + player + "/volume/" + volume
-#
-# http://10.0.0.21:5005/kontor/volume/30
-# http://10.0.0.21:5005/kontor/play
-# http://10.0.0.21:5005/kontor/playlist/Snilles%20List
+sonosaction = "http://" + sonosapiip + ":" + sonosapiport + "/" + player + "/"
 
 # The directory where the sounds are located.
 sounddir = "Sounds"
@@ -140,20 +167,18 @@ sounddir = "Sounds"
 started = 0
 read = 0
 
-# Loking the chest
-print("The hunt is on! Locking the chest!")
-
+# Locking the chest.
+print(consoleprestartmessae)
 kit.servo[0].angle = lockangle
 
 try:
-    GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
-    # Create an object hx which represents your real hx711 chip
-    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
+    GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering.
+    # Create an object hx which represents your real hx711 chip.
+    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'.
     hx = HX711(dout_pin=21, pd_sck_pin=20)
-    # measure tare and save the value as offset for current channel
-    # and gain selected. That means channel A and gain 128
+    # measure tare and save the value as offset for current channel and gain selected. That means channel A and gain 128
     err = hx.zero()
-    # check if successful
+    # check if successful.
     if err:
         raise ValueError('Tare is unsuccessful.')
 
@@ -161,7 +186,7 @@ try:
     GPIO.setup(lidpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(secretpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    print("I will now start to keep track of collected coins in an infinite loop. To exit press 'CTRL + C' or collect", goal, "coins to unlock the chest")
+    print(consolestartmessage[0], goal, consolestartmessage[1])
 
     # Calculating the scale of one single coin.
     hx.set_scale_ratio(ratio)
@@ -171,7 +196,7 @@ try:
 
         # Checks what's on the scale (returns only integers and no negative numbers).
         val = max(0, int(hx.get_weight_mean(5)))
-        # Make sure that the "value" (show and spoken) of the collected coins don't overshoot the goal (if that's allowed).
+        # Make sure that the "value" (shown and spoken) of the collected coins don't overshoot the goal (if that's allowed).
         if overgoalok == 1:
             if val >= goal:
                 val = goal
@@ -179,23 +204,25 @@ try:
         # When lid opens, this happens...
         if GPIO.input(lidpin) == GPIO.LOW:
             if read == 1:
-                print("Collected coins so far: " + str(val))
+                print(consolecoinmessage + str(val))
                 if mmpost == 1:
-                    # Show the coin module and the message on the MagicMirror
+                    # Show the coin module and the message on the MagicMirror.
                     moduleresponse = urllib.request.urlopen(hidecoinmodule2)
                     time.sleep(1)
                     moduleresponse = urllib.request.urlopen(showcoinmodule1)
-                    iftttresponse = requests.post(url, json={'message': 'Ni har hittat ' + str(val) + ' av 100 piratmynt. Fortsätt leta...', 'displaySeconds': mmtime, 'size': mmsize})
-                
+                    iftttresponse = requests.post(ifttturl, json={'message': iftttcoinmessage[0] + str(val) + iftttcoinmessage[1], 'displaySeconds': mmtime, 'size': mmsize})
+
+                # This happens if the lid is opened the first time (no coins are inserted yet).
                 if val == 0:
-                    # Display start message on MagicMirror
+                    # Display start message on MagicMirror.
                     if mmpost == 1:
                         moduleresponse = urllib.request.urlopen(hideprestartmodule)
                         moduleresponse = urllib.request.urlopen(showstartmodule)
                         moduleresponse = urllib.request.urlopen(hidecoinmodule2)
 
-                        iftttresponse = requests.post(url, json={'message': 'För att låsa upp kistan helt måste ni samla ihop minst 100 piratmynt och peta in dem genom glipan som blir när man försöker öppna kistlocket!', 'displaySeconds': mmtime, 'size': mmsize})
-                    # Only play sounds when playsound is 1
+                        iftttresponse = requests.post(ifttturl, json={'message': iftttstartmessage, 'displaySeconds': mmtime, 'size': mmsize})
+
+                    # Only play sounds when playsound is 1.
                     if playsound == 1:
                         # Avoids the first message to be played when the program starts with the lid closed.
                         if info == 0:
@@ -215,7 +242,8 @@ try:
                             os.system("aplay -q " + sounddir + "/Explain-03.wav")
                             os.system("aplay -q " + sounddir + "/Haha-03.wav")
                             info = 1
-
+                
+                # Playing the chest count sounds.
                 if playsound == 1:
                     # Setting up the count sound sample.
                     fullpath = sounddir + "/" + str(val) + ".wav"
@@ -226,7 +254,7 @@ try:
                         os.system("aplay -q " + fullpath)
                         os.system("aplay -q " + sounddir + "/Count-02.wav")
 
-                    # Generates a random number between 0-8
+                    # Generates a random number between 0-8.
                     rand = random.randint(1, 6)
                     # Plays a laugh if the random number matches.
                     if rand == 2:
@@ -239,7 +267,7 @@ try:
                 # Only play once. The lid has to be closed and opened again to play again.
                 read = 0
 
-                # Hide the coin module 1 and show coin module 2 on the MagicMirror
+                # Hide the coin module 1 and show coin module 2 on the MagicMirror.
                 if mmpost == 1:
                     moduleresponse = urllib.request.urlopen(hidecoinmodule1)
                     time.sleep(2)
@@ -259,24 +287,28 @@ try:
         else:
             kit.servo[0].angle = lockangle
             if started == 0:
-                # Display Pre start message on the MagicMirror
+                # Display pre start message on the MagicMirror.
                 if mmpost == 1:
-                    # Hiding modules
+                    # Hiding the "everyone modules".
                     moduleresponse = urllib.request.urlopen(hidemodulebar)
                     moduleresponse = urllib.request.urlopen(hidehideall)
                     moduleresponse = urllib.request.urlopen(hidetouchnavigation)
+                    moduleresponse = urllib.request.urlopen(hidecurrentweather)
+                    moduleresponse = urllib.request.urlopen(hideweatherforecast)
                     
-                    # Showing Pre-Start Profile
+                    # Showing pre start Profile.
                     profileresponse = urllib.request.urlopen(startprofile)
                     moduleresponse = urllib.request.urlopen(showprestartmodule)
                     moduleresponse = urllib.request.urlopen(showcoinmodule2)
+                    moduleresponse = urllib.request.urlopen(showiftttmodule)
                     
                     # Posting start message.
-                    iftttresponse = requests.post(url, json={'message': 'Jakten på piratmynten har börjat!!', 'displaySeconds': mmtime, 'size': mmsize})
-                # Start sonos playr
-                If playonsonos == 1
-                    #urllib.request.urlopen("http://" sonosapiip + ":" + sonosapiport + "/" + player + "/volume/" + volume)
-                    #urllib.request.urlopen("http://" sonosapiip + ":" + sonosapiport + "/" + player + "/playlist/" + playlist)
+                    iftttresponse = requests.post(ifttturl, json={'message': iftttprestartmessage, 'displaySeconds': mmtime, 'size': mmsize})
+
+                # Sets the SONOS volume and start the selected playlist on the selected player.
+                if playonsonos == 1:
+                    urllib.request.urlopen(sonosaction + "volume/" + str(volume))
+                    urllib.request.urlopen(sonosaction + "playlist/" + playlist)
 
                 started = 1
 
@@ -287,21 +319,39 @@ try:
 # At the end of everything, exit cleanly.
 except (KeyboardInterrupt, SystemExit):
     kit.servo[0].angle = unlockangle
-    print("Chest is now unlocked, bye...")
+    print(consoleendmessage)
 
+    # Displays the "Win" message on the MagicMirror.
     if mmpost == 1:
         profileresponse = urllib.request.urlopen(endprofile)
-        iftttresponse = requests.post(url, json={'message': 'Grattis!! Ni har nu hittat mer än 100 piratmynt! Kistan är upplåst, Hoppas godiset smakar!', 'displaySeconds': mmendtime, 'size': mmsize})
-        
+        iftttresponse = requests.post(ifttturl, json={'message': iftttendmessage, 'displaySeconds': mmendtime, 'size': mmsize})
+
+    # Plays the "win" sound from the Chest.
     if playsound == 1:
         os.system("aplay -q " + sounddir + "/Done-01.wav")
 
+    # Decrees the SONOS volume until it's 1 and then stops playing.
+    if playonsonos == 1:
+        # Decrees loop.
+        while (volume > 1):
+            volume -= 1
+            urllib.request.urlopen(sonosaction+ "volume/" + str(volume))
+            time.sleep(0.1)
+        # Stops the playback.
+        urllib.request.urlopen(sonosaction + "pause")
+    
+    # Setting everything on the MagicMirror back to normal.
     if mmpost == 1:
         time.sleep(waittime)
 
         # Showing modules and switching to the normal profile.
         profileresponse = urllib.request.urlopen(normalprofile)
         time.sleep(3)
+        
+        # Hiding the IFTTT module and shows the "everyone modules".
+        moduleresponse = urllib.request.urlopen(hideiftttmodule)
+        moduleresponse = urllib.request.urlopen(showcurrentweather)
+        moduleresponse = urllib.request.urlopen(showweatherforecast)
         moduleresponse = urllib.request.urlopen(showmodulebar)
         moduleresponse = urllib.request.urlopen(showhideall)
         moduleresponse = urllib.request.urlopen(showtouchnavigation)
